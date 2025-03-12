43302195063888bc479f3ba9b3a3fae1c5fff51c
Luisf66 <Luisf311220@gmail.com>
2025-03-12 20:15:54 -0300

    APIview RetrieveUpdateDestroy

--- genres/views.py
+++ genres/views.py
@@ -1,8 +1,4 @@
-import json
-from django.http import JsonResponse
 from genres.models import Genre
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from genres.serializers import GenreSerializer
 # Create your views here.
@@ -11,17 +7,7 @@ class GenreCreateListView(generics.ListCreateAPIView):
     serializer_class = GenreSerializer
 
 
-@csrf_exempt
-def genre_detail_view(request,pk):
-    genre = get_object_or_404(Genre, pk=pk)
-    if request.method == 'GET':
-        data = {'id': genre.id, 'name': genre.name}
-        return JsonResponse(data)
-    elif request.method == 'PUT':
-        data = json.loads(request.body.decode('utf-8'))
-        genre.name = data['name']
-        genre.save()
-        return JsonResponse({'id': genre.id, 'name': genre.name})
-    elif request.method == 'DELETE':
-        genre.delete()
-        return JsonResponse({'message': 'Gênero excluído com sucesso'}, status=204)
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
