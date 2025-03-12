a130013965b421da05ec602ddefd8749a00b6d33
Luisf66 <Luisf311220@gmail.com>
2025-03-12 15:05:21 -0300

    Uso de serializer + APIview

--- genres/views.py
+++ genres/views.py
@@ -4,21 +4,13 @@ from genres.models import Genre
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from genres.serializers import GenreSerializer
 # Create your views here.
-@csrf_exempt
-def genre_view(request):
-    if request.method == 'GET':
-        genres = Genre.objects.all()
-        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-        return JsonResponse(data, safe=False)
-    elif request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8'))
-        new_genre = Genre(name=data['name'])
-        new_genre.save()
-        return JsonResponse(
-            {'id': new_genre.id, 'name': new_genre.name},
-            status=201
-        )
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
 @csrf_exempt
 def genre_detail_view(request,pk):
     genre = get_object_or_404(Genre, pk=pk)
