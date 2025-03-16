a49fac906e682cd39add180e6c4d802b78ac87e9
Luisf66 <Luisf311220@gmail.com>
2025-03-16 17:54:22 -0300

    validacao nos serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -6,3 +6,9 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self,value):
+        if value.year < 1990:
+            raise serializers.ValidationError('A data não deve ser menor que 1990')
+        return value
+    
